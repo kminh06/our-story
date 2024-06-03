ba48df342bc8ec81e991334b83e0c3d7a2a62e5b
KMinh <daukhacminh006@gmail.com>
2024-06-03 10:30:44 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# The Story of Our World
+
+1. In the beginning there was Earth, and animals, and there was man.
+2. Man populated the Earth plenty, and they prospered.
+3. Man built technologies, and cities, and weapons.
+4. For thousands of years, fantastical civilizations thrived.
+5. Then they angered the Gods, and a Great Flood sank the world.
+6. Those that tended herds and farmed crops lived high up, and they survived.
+7. They then populated the Earth plenty, and they prospered.
---
+++ src/consts.ts
@@ -0,0 +1,32 @@
+import type { Site, Page, Links, Socials } from '@types'
+
+// Global
+export const SITE: Site = {
+  TITLE: 'The Story of Man',
+  DESCRIPTION: 'A poem about the story of our existence.',
+  AUTHOR: 'Khac Minh Dau',
+}
+
+// Links
+export const LINKS: Links = [
+  {
+    TEXT: 'Home',
+    HREF: '/',
+  },
+]
+
+// Socials
+export const SOCIALS: Socials = [
+  {
+    NAME: 'Email',
+    ICON: 'email',
+    TEXT: 'daukhacminh006@gmail.com',
+    HREF: 'mailto:daukhacminh006@gmail.com',
+  },
+  {
+    NAME: 'Github',
+    ICON: 'github',
+    TEXT: 'kminh06',
+    HREF: 'https://github.com/kminh06/our-story',
+  },
+]
